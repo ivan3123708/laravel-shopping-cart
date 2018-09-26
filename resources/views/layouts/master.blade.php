7bcae8d04920530f286d1132ead1f9e6005171ca
ivan3123708 <ivan3123708@gmail.com>
2018-09-26 12:32:15 +0200

    Product index view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,14 +3,20 @@
 <head>
   <meta charset="utf-8" />
   <title>@yield('title')</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ URL::to('css/custom.css') }}">
   @yield('styles')
 </head>
 <body>
+  @include('partials.header')
+
+  <div class="container">
+    @yield('content')
+  </div>
   
-  @yield('content')
   <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   @yield('scripts')
 </body>
 </html>
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,5 +5,86 @@
 @endsection
 
 @section('content')
-  <h1>IT WORKS!</h1>
+  <div class="row">
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="https://www.richardkmorgan.com/wp-content/uploads/altered-carbon_UK_Pb.jpg" alt="...">
+        <div class="caption">
+          <h3>Product Title</h3>
+          <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tenetur, eligendi autem doloremque ea labore praesentium eius ipsum cupiditate minima quibusdam ut consectetur sed officia aperiam, ratione consequuntur pariatur magnam!</p>
+          <div class="clearfix">
+            <div class="pull-left price">$12</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="https://www.richardkmorgan.com/wp-content/uploads/altered-carbon_UK_Pb.jpg" alt="...">
+        <div class="caption">
+          <h3>Product Title</h3>
+          <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tenetur, eligendi autem doloremque ea labore praesentium eius ipsum cupiditate minima quibusdam ut consectetur sed officia aperiam, ratione consequuntur pariatur magnam!</p>
+          <div class="clearfix">
+            <div class="pull-left price">$12</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="https://www.richardkmorgan.com/wp-content/uploads/altered-carbon_UK_Pb.jpg" alt="...">
+        <div class="caption">
+          <h3>Product Title</h3>
+          <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tenetur, eligendi autem doloremque ea labore praesentium eius ipsum cupiditate minima quibusdam ut consectetur sed officia aperiam, ratione consequuntur pariatur magnam!</p>
+          <div class="clearfix">
+            <div class="pull-left price">$12</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="https://www.richardkmorgan.com/wp-content/uploads/altered-carbon_UK_Pb.jpg" alt="...">
+        <div class="caption">
+          <h3>Product Title</h3>
+          <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tenetur, eligendi autem doloremque ea labore praesentium eius ipsum cupiditate minima quibusdam ut consectetur sed officia aperiam, ratione consequuntur pariatur magnam!</p>
+          <div class="clearfix">
+            <div class="pull-left price">$12</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="https://www.richardkmorgan.com/wp-content/uploads/altered-carbon_UK_Pb.jpg" alt="...">
+        <div class="caption">
+          <h3>Product Title</h3>
+          <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tenetur, eligendi autem doloremque ea labore praesentium eius ipsum cupiditate minima quibusdam ut consectetur sed officia aperiam, ratione consequuntur pariatur magnam!</p>
+          <div class="clearfix">
+            <div class="pull-left price">$12</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="https://www.richardkmorgan.com/wp-content/uploads/altered-carbon_UK_Pb.jpg" alt="...">
+        <div class="caption">
+          <h3>Product Title</h3>
+          <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tenetur, eligendi autem doloremque ea labore praesentium eius ipsum cupiditate minima quibusdam ut consectetur sed officia aperiam, ratione consequuntur pariatur magnam!</p>
+          <div class="clearfix">
+            <div class="pull-left price">$12</div>
+            <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
